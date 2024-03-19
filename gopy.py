40b4b9870d0df71e88f8076f045a05453f1a1e62
MaCAT <138701551+maximizemaxwell@users.noreply.github.com>
2024-03-19 10:50:46 +0900

    Create gopy.py

---
+++ gopy.py
@@ -0,0 +1,19 @@
+name = input()
+jumin = str(input)
+year = jumin[0:2]
+month_num = jumin[2:4]
+date = jumin[4:6]  
+if year<=24:
+    year = jumin[0] + 2000
+else:
+    year = jumin[0] + 1900
+
+month_dic = {
+    01: Jan, 02: Feb, 03: Mar, 04: April, 05: May, 06: June,
+    07: July, 08: Aug, 09: Sep, 10: Oct, 11: Nov, 12: Dec }
+
+if month_num in month_dic:
+    month_name = month_dic[month_num]
+print('your name is {}'.format(name))
+print('you were born in {}/{}/{}'. format(year,month_name,date))
+print('you are {} years old'.format(2024-year))
